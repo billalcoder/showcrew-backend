e83e5568ad9c7d72f0b42fbaec0d4ffeaccf4675
billalcoder <billalshekhani10@gmail.com>
2025-08-21 12:36:53 +0530

    fixed the sid

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { productModel } from "../model/productModel.js";
+import { SessionModel } from "../model/session.js";
 import { userModel } from "../model/userModel.js";
 
 const router = express.Router();
 import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import multer from "multer";
+import { SessionModel } from "../model/session.js";
 
 const upload = multer({ storage: multer.memoryStorage() });
 
